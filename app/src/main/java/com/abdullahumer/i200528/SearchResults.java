c09f60ad76d65d28c757084d75c2c45102dff4f8
aybeedee <abdullahumer575@gmail.com>
2023-10-23 11:23:59 +0500

    add filter functionality to search results

--- app/src/main/java/com/abdullahumer/i200528/SearchResults.java
+++ app/src/main/java/com/abdullahumer/i200528/SearchResults.java
@@ -14,6 +14,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -24,6 +25,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -68,37 +70,141 @@ public class SearchResults extends AppCompatActivity {
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        // for featured items and your items
-        mDatabase.child("items").addChildEventListener(new ChildEventListener() {
+        filterSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
-            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
-                Item itemObject = snapshot.getValue(Item.class);
+                if (i == 0) {
 
-                if (itemObject.getItemName().toLowerCase().contains(searchInput.toLowerCase())) {
-                    searchList.add(itemObject);
+                    searchList.clear();
                     searchAdapter.notifyDataSetChanged();
+
+                    mDatabase.child("items").addChildEventListener(new ChildEventListener() {
+
+                        @Override
+                        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                            Item itemObject = snapshot.getValue(Item.class);
+
+                            if (itemObject.getItemName().toLowerCase().contains(searchInput.toLowerCase())) {
+
+                                searchList.add(itemObject);
+                                searchAdapter.notifyDataSetChanged();
+                            }
+                        }
+
+                        @Override
+                        public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        }
+
+                        @Override
+                        public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                        }
+
+                        @Override
+                        public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+
+                else if (i == 1) {
+
+                    searchList.clear();
+                    searchAdapter.notifyDataSetChanged();
+
+                    Query itemsLowToHigh = mDatabase.child("items").orderByChild("rate");
+
+                    itemsLowToHigh.addChildEventListener(new ChildEventListener() {
+
+                        @Override
+                        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                            Item itemObject = snapshot.getValue(Item.class);
+
+                            if (itemObject.getItemName().toLowerCase().contains(searchInput.toLowerCase())) {
+
+                                searchList.add(itemObject);
+                                searchAdapter.notifyDataSetChanged();
+                            }
+                        }
+
+                        @Override
+                        public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        }
+
+                        @Override
+                        public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                        }
+
+                        @Override
+                        public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+
+                else if (i == 2) {
+
+                    searchList.clear();
+                    searchAdapter.notifyDataSetChanged();
+
+                    Query itemsLowToHigh = mDatabase.child("items").orderByChild("rate");
+
+                    itemsLowToHigh.addChildEventListener(new ChildEventListener() {
+
+                        @Override
+                        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                            Item itemObject = snapshot.getValue(Item.class);
+
+                            if (itemObject.getItemName().toLowerCase().contains(searchInput.toLowerCase())) {
+
+                                searchList.add(0, itemObject);
+                                searchAdapter.notifyDataSetChanged();
+                            }
+                        }
+
+                        @Override
+                        public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        }
+
+                        @Override
+                        public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                        }
+
+                        @Override
+                        public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
                 }
             }
 
             @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
         });
@@ -109,7 +215,6 @@ public class SearchResults extends AppCompatActivity {
 
                 Intent intent = new Intent(SearchResults.this, MainActivity.class);
                 startActivity(intent);
-
             }
         });
     }
